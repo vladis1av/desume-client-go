459783e56fa9b05d3ce2de574ee7becf478bb4fe
vladis1av <vladfrolov.dev@gmail.com>
2024-08-16 16:23:56 +0700

    add limiter

--- desume/client.go
+++ desume/client.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/url"
 	"path"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 // defaultBaseURL constant defines the default API base URL that will be used by the client unless another URL is specified.
@@ -15,10 +18,15 @@ const (
 	defaultBaseURL = "https://desu.win/manga/api/"
 )
 
+var (
+	ErrRateLimitExceeded = errors.New("rate limit exceeded")
+)
+
 // Client structure represents the API client that will be used to send HTTP requests to the API.
 type Client struct {
-	baseURL    string
-	httpClient *http.Client
+	baseURL     string
+	httpClient  *http.Client
+	rateLimiter *rate.Limiter
 }
 
 // Params type represents the request parameters that will be passed to the API URL.
@@ -42,41 +50,49 @@ func WithBaseURL(baseURL string) Option {
 	}
 }
 
-// WithBaseURL feature option sets the base timeout for the Client instance
+// WithTimeout feature option sets the timeout for the HTTP client
 func WithTimeout(timeout time.Duration) Option {
 	return func(c *Client) {
 		c.httpClient.Timeout = timeout
 	}
 }
 
-// WithBaseURL feature option sets the base MaxIdleConns for the Client instance
+// WithMaxIdleConns feature option sets the maximum number of idle connections for the HTTP client
 func WithMaxIdleConns(maxIdleConns int) Option {
 	return func(c *Client) {
 		c.httpClient.Transport.(*http.Transport).MaxIdleConns = maxIdleConns
 	}
 }
 
-// WithBaseURL feature option sets the base IdleConnTimeout for the Client instance
+// WithIdleConnTimeout feature option sets the idle connection timeout for the HTTP client
 func WithIdleConnTimeout(idleConnTimeout time.Duration) Option {
 	return func(c *Client) {
 		c.httpClient.Transport.(*http.Transport).IdleConnTimeout = idleConnTimeout
 	}
 }
 
+// WithTLSHandshakeTimeout feature option sets the TLS handshake timeout for the HTTP client
 func WithTLSHandshakeTimeout(tlsHandshakeTimeout time.Duration) Option {
 	return func(c *Client) {
 		c.httpClient.Transport.(*http.Transport).TLSHandshakeTimeout = tlsHandshakeTimeout
 	}
 }
 
-// WithBaseURL feature option sets the base DisableCompression for the Client instance
+// WithDisableCompression feature option sets the disable compression flag for the HTTP client
 func WithDisableCompression(disableCompression bool) Option {
 	return func(c *Client) {
 		c.httpClient.Transport.(*http.Transport).DisableCompression = disableCompression
 	}
 }
 
-// DESUME Client instance
+// WithRateLimiter feature option sets the rate limiter for the Client instance
+func WithRateLimiter(rps int, burst int) Option {
+	return func(c *Client) {
+		c.rateLimiter = rate.NewLimiter(rate.Limit(rps), burst)
+	}
+}
+
+// NewClient creates a new DESUME Client instance
 func NewClient(options ...Option) *Client {
 	httpClient := &http.Client{
 		Timeout: 15 * time.Second,
@@ -106,6 +122,12 @@ func (c *Client) sendRequest(ctx context.Context, method string, endpoint string
 		ctx = context.Background()
 	}
 
+	if c.rateLimiter != nil {
+		if !c.rateLimiter.Allow() {
+			return nil, ErrRateLimitExceeded
+		}
+	}
+
 	u, err := url.Parse(c.baseURL)
 	if err != nil {
 		return nil, err
@@ -127,6 +149,7 @@ func (c *Client) sendRequest(ctx context.Context, method string, endpoint string
 		if err != nil {
 			return nil, err
 		}
+		req.Header.Set("Content-Type", "application/json")
 	} else {
 		req, err = http.NewRequestWithContext(ctx, method, u.String(), nil)
 		if err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vladis1av/desume-client-go
 
 go 1.21.5
+
+require golang.org/x/time v0.6.0
